ca5039e4e0ce945f09c0a477b8d92d4d4d716785
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-04-29 23:07:08 -0300

    update

--- .screenlayout/monitor.sh
+++ .screenlayout/monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1368x768 --pos 1600x0 --rotate normal --output HDMI1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output eDP-1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output HDMI-1 --on
